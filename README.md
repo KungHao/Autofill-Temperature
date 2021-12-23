4105a46e8ac50596bba3bf329985bfcf7240608e
KungHao <james19961119@gmail.com>
2021-12-23 23:20:11 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Auto-fill Temperature in TSMC
+
+---
+
+## Function
+* 設定自動填表時間
+* 自動生成體溫
+* 設定單一/數個工號
+* 回傳填表成功時間、體溫及工號
+* 接上Line chat bot
--- autofill_temperature.py
+++ autofill_temperature.py
@@ -1,32 +1,15 @@
+from datetime import datetime
 from selenium import webdriver
-
 from selenium.webdriver.chrome.options import Options
-
 import random
-
 import time
 
-
-
 options = webdriver.ChromeOptions()
-
 options.add_argument("--disable-notifications")
 
-
-
-
-
-
-
 def fill(empid, temp):
-
     browser = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
-
     browser.get('https://zh.surveymonkey.com/r/EmployeeHealthCheck') 
-
-
-
-
 
     radiobutton = browser.find_elements_by_class_name("radio-button-display")
 
@@ -34,64 +17,38 @@ def fill(empid, temp):
 
     checkbox = browser.find_elements_by_class_name("checkbox-button-display")
 
-
-
     radiobutton_checked = [0,1,5,6,8]
 
-    
-
     for i in radiobutton_checked:
-
         radiobutton[i].click()
 
-
-
     checkbox[0].click()
-
-
-
     info = [empid, temp]
 
-
-
     for i in range(len(textbox)):
-
         textbox[i].send_keys(str(info[i]))
-
-        
-
-    
 
     time.sleep(1)
 
-
-
     browser.find_elements_by_class_name("btn")[0].click()
-
-
-
     browser.close()
 
+def temperatureGen():
+    return round(random.uniform(35.6, 37.3), 1)
 
-
-def tempGen():
-
-    return round(random.uniform(36.5,37.3),1)
-
-
+def get_now():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
 
 if __name__ == "__main__":    
+    # IDs = ["127727","123843","125800","127744", "122908"]
+    IDs = ['125800']
 
-
-
-    emp = ["127727","123843","125800","127744", "122908"]
-
-
-
-    for i in emp:
-
-        temp = tempGen()
-
-        fill(i, temp)
-
-        print(i, "temp: ", temp)
+    for i in IDs:
+        temp = temperatureGen()
+        # fill(i, temp)
+        current_time = get_now()
+        print('ID: ', i)
+        print("temp: ", temp)
+        print("Current Time =", current_time)
