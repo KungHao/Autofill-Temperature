f21c5f6251a0b235f5bff1dd4bd3d97ffc8bf5bb
KungHao <james19961119@gmail.com>
2021-12-26 10:31:01 +0800

    test

--- autofill_temperature.py
+++ autofill_temperature.py
@@ -4,7 +4,6 @@ import random
 from config import *
 from datetime import datetime
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
